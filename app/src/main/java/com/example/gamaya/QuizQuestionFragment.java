1fcce6f929c1c354087944f70c896580a7cf873d
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-27 08:29:07 +0700

    create question screen for quiz

---
+++ app/src/main/java/com/example/gamaya/QuizQuestionFragment.java
@@ -0,0 +1,26 @@
+package com.example.gamaya;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class QuizQuestionFragment extends Fragment {
+
+    public static QuizQuestionFragment newInstance(String param1, String param2) {
+        QuizQuestionFragment fragment = new QuizQuestionFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_question, container, false);
+    }
+}
