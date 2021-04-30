a1e58df8362b6ae8777878f45e0ddccfc59731f0
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-30 17:03:33 +0700

    layouting tebak gambar

---
+++ app/src/main/java/com/example/gamaya/TebakGambarActivity.java
@@ -0,0 +1,12 @@
+package com.example.gamaya;
+
+import android.os.Bundle;
+
+public class TebakGambarActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tebak_gambar);
+    }
+}
