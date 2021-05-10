c879a47852b0b99657cae8822ec7871175a1e70c
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-10 11:36:40 +0700

    add action btn back

--- app/src/main/java/com/example/gamaya/PetunjukActivity.java
+++ app/src/main/java/com/example/gamaya/PetunjukActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.gamaya.databinding.ActivityPetunjukBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class PetunjukActivity extends AppCompatActivity {
+
+    private ActivityPetunjukBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_petunjuk);
+        binding = ActivityPetunjukBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnBack.setOnClickListener(v -> {
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle("Keluar Dari Tebak Gambar")
+                    .setMessage("Anda yakin ingin mengakhiri tebak gambar?")
+                    .setPositiveButton("Keluar", (dialog, which) -> onBackPressed())
+                    .setNegativeButton("Lanjut Tebak Gambar", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        });
     }
 }
--- app/src/main/java/com/example/gamaya/ProfileActivity.java
+++ app/src/main/java/com/example/gamaya/ProfileActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.gamaya.databinding.ActivityPetunjukBinding;
+import com.example.gamaya.databinding.ActivityProfileBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private ActivityProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnBack.setOnClickListener(v -> {
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle("Keluar Dari Tebak Gambar")
+                    .setMessage("Anda yakin ingin mengakhiri tebak gambar?")
+                    .setPositiveButton("Keluar", (dialog, which) -> onBackPressed())
+                    .setNegativeButton("Lanjut Tebak Gambar", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        });
     }
 }
